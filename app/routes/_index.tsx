0b40d9a0c1e059bbfa6bdb2f8c85457f654b5426
sarahfcader <sarah.f.cader@gmail.com>
2023-06-24 18:58:00 -0700

    Added typescript type for Supabase db.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,24 @@
+import type { LoaderArgs } from "@remix-run/node";
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
+
+export const loader = async ({}: LoaderArgs) => {
+  const { data } = await supabase.from("messages").select();
+  return { messages: data || [] };
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Lullaby" },
+    { name: "A simple messaging app made with Remix."},
   ];
 };
 
 export default function Index() {
+  // Hook, type inferred from return type of loader()
+  const { messages } = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <pre>{JSON.stringify(messages, null, 2)}</pre>
   );
 }
